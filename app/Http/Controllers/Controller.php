6ecb705e9a92a67df05dcdffa1d41b95ab9b42be
Medhat Elmasry <medhat@elmasry.ca>
2025-03-27 12:11:47 -0700

    fixed urls on parent controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,9 +7,9 @@ use OpenApi\Attributes as OA;
 #[
     OA\Info(version: "1.0.0", description: "School API Documentation", title: "School API Documentation"),
     OA\Server(url: 'http://127.0.0.1:8000/api', description: "local server"), 
-    OA\Server (url: 'https://laravel-sqlite-api-swagger-mze.azurewebsites.net', description: "staging server"), 
-    OA\Server(url: 'https://laravel-sqlite-api-swagger-mze.azurewebsites.net', description: "production server"),
-    OA\SecurityScheme (securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header"),
+    OA\Server(url: 'https://laravel-sqlite-api-swagger-mze.azurewebsites.net/api', description: "staging server"), 
+    OA\Server(url: 'https://laravel-sqlite-api-swagger-mze.azurewebsites.net/api', description: "production server"),
+    OA\SecurityScheme(securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header"),
 ]
 abstract class Controller
 {
